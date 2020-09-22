a9a0fe2c7413e187d7a08cbbbc478425bdc8d679
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-22 12:32:07 -0500

    Added gapi usage in home page

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,14 +1,44 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import useGAPI from '../../hooks/useGAPI';
 import VideoList from '../../components/VideoList';
 
 import './Home.styles.css';
 
 function HomePage() {
   const sectionRef = useRef(null);
+  const [keyword, setKeyword] = useState('react');
+  const gapi = useGAPI();
+
+  function searchByKeyword(keyword, maxResults = 10) {
+    gapi.client.youtube.search
+      .list({
+        part: ['snippet'],
+        maxResults,
+        q: keyword,
+        type: ['video'],
+      })
+      .then(
+        function (response) {
+          // Handle the results here (response.result has the parsed body).
+          console.log('Response', response);
+        },
+        function (err) {
+          console.error('Execute error', err);
+        }
+      );
+  }
 
   return (
     <section className="homepage" ref={sectionRef}>
       <h1>Welcome to homepage!</h1>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(event) => setKeyword(event.target.value)}
+      />
+      <button type="button" onClick={() => searchByKeyword(keyword, 7)}>
+        Search
+      </button>
       <VideoList />
     </section>
   );
