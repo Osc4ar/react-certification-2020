53d67b20858fe74f9e89c62e967652c3f7a7058a
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-22 12:22:52 -0500

    Deleted gapi from this page

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
-import useYoutube from '../../hooks/useYoutube';
+import React from 'react';
 
 import './Video.styles.css';
 
 function VideoPage() {
-  const [keyword] = useState('react');
-  const [searchByKeyword] = useYoutube();
-
-  const search = () => searchByKeyword(keyword);
-
   return (
     <section className="videopage">
       <h1>Welcome to your video!</h1>
-      <button type="button" onClick={search}>
-        Search
-      </button>
     </section>
   );
 }
