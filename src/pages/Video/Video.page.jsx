6fc9d26b50d84ca76bd61a41b55df4acf29f90a8
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-21 23:22:17 -0500

    Testing hook, some problems when loading the gapi object

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useYoutube from '../../hooks/useYoutube';
 
 import './Video.styles.css';
 
 function VideoPage() {
+  const [keyword] = useState('react');
+  const [searchByKeyword] = useYoutube();
+
+  const search = () => searchByKeyword(keyword);
+
   return (
     <section className="videopage">
       <h1>Welcome to your video!</h1>
+      <button type="button" onClick={search}>
+        Search
+      </button>
     </section>
   );
 }
