a9afc22bbbc9f47ca9816d41702589315f106aa8
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-25 09:30:42 -0500

    Moved GAPI to standard field

--- src/utils/hooks/useGAPI.js
+++ src/utils/hooks/useGAPI.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 
-export default function useGAPI(url, key) {
+const API_URL = 'https://apis.google.com/js/client.js';
+const API_KEY = 'AIzaSyD-LNjx3t5Yfr_qGcqY0KQE4hHGvWC71W0';
+
+export default function useGAPI() {
   const [gapi, setGAPI] = useState(null);
 
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = url;
+    script.src = API_URL;
     script.onload = () => {
       window.gapi.load('client', () => {
-        window.gapi.client.setApiKey(key);
+        window.gapi.client.setApiKey(API_KEY);
         window.gapi.client.load('youtube', 'v3', () => {
           setGAPI(window.gapi);
         });
@@ -21,7 +24,7 @@ export default function useGAPI(url, key) {
     return () => {
       document.body.removeChild(script);
     };
-  }, [url, key]);
+  });
 
   return gapi;
 }
