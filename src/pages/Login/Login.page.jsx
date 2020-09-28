50fabdc173f3038e8c74db9c7e96c25c449f7a87
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-28 17:51:07 -0500

    Added login credentials

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 
 import { useAuth } from '../../providers/Auth';
@@ -7,27 +7,49 @@ import './Login.styles.css';
 function LoginPage() {
   const { login } = useAuth();
   const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('Welcome back!');
 
   function authenticate(event) {
     event.preventDefault();
-    login();
-    history.push('/');
+    if (
+      username === process.env.REACT_APP_USERNAME &&
+      password === process.env.REACT_APP_PASSWORD
+    ) {
+      login();
+      history.push('/');
+    } else {
+      setMessage('Wrong credentials, try again');
+    }
   }
 
   return (
     <section className="login">
-      <h1>Welcome back!</h1>
+      <h1>{message}</h1>
       <form onSubmit={authenticate} className="login-form">
         <div className="form-group">
           <label htmlFor="username">
             <strong>username </strong>
-            <input required type="text" id="username" />
+            <input
+              required
+              type="text"
+              id="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </label>
         </div>
         <div className="form-group">
           <label htmlFor="password">
             <strong>password </strong>
-            <input required type="password" id="password" />
+            <input
+              required
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </label>
         </div>
         <button type="submit">login</button>
