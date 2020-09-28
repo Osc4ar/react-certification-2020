67203da4f9178d398ac0d8064654d357c037c01a
oscar.castillo <oscar.castillo@wizeline.com>
2020-09-28 18:11:41 -0500

    Added search to navbar

--- src/components/Nav/Nav.component.jsx
+++ src/components/Nav/Nav.component.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../providers/Auth';
+import VideosContext from '../../utils/state/VideosContext';
+import { formatVideosList } from '../../utils/youtube';
+import useGAPI from '../../utils/hooks/useGAPI';
 
 import './Nav.styles.css';
 
 function Nav() {
   const history = useHistory();
   const { authenticated, logout } = useAuth();
+  const { setVideos } = useContext(VideosContext);
+  const [keyword, setKeyword] = useState('react');
+  const gapi = useGAPI();
+
+  const searchByKeyword = (maxResults = 10) => {
+    gapi.client.youtube.search
+      .list({
+        part: ['snippet'],
+        maxResults,
+        q: keyword,
+        type: ['video'],
+      })
+      .then(
+        function (response) {
+          setVideos(formatVideosList(response.result.items));
+        },
+        function (err) {
+          console.error('Execute error', err);
+        }
+      );
+  };
 
   function deAuthenticate(event) {
     event.preventDefault();
@@ -23,6 +47,16 @@ function Nav() {
           🏠
         </span>
       </Link>
+      <div>
+        <input
+          type="text"
+          value={keyword}
+          onChange={(event) => setKeyword(event.target.value)}
+        />
+        <button type="button" onClick={() => searchByKeyword(5)}>
+          Search
+        </button>
+      </div>
       {authenticated ? (
         <>
           <Link to="/favorites">
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,47 +1,15 @@
-import React, { useRef, useState, useContext } from 'react';
+import React, { useRef } from 'react';
 
-import VideosContext from '../../utils/state/VideosContext';
-import useGAPI from '../../utils/hooks/useGAPI';
-import { formatVideosList } from '../../utils/youtube';
 import VideoList from '../../components/VideoList';
 
 import './Home.styles.css';
 
 function HomePage() {
   const sectionRef = useRef(null);
-  const [keyword, setKeyword] = useState('react');
-  const { setVideos } = useContext(VideosContext);
-  const gapi = useGAPI();
-
-  const searchByKeyword = (maxResults = 10) => {
-    gapi.client.youtube.search
-      .list({
-        part: ['snippet'],
-        maxResults,
-        q: keyword,
-        type: ['video'],
-      })
-      .then(
-        function (response) {
-          setVideos(formatVideosList(response.result.items));
-        },
-        function (err) {
-          console.error('Execute error', err);
-        }
-      );
-  };
 
   return (
     <section className="homepage" ref={sectionRef}>
       <h1>Welcome to homepage!</h1>
-      <input
-        type="text"
-        value={keyword}
-        onChange={(event) => setKeyword(event.target.value)}
-      />
-      <button type="button" onClick={() => searchByKeyword(5)}>
-        Search
-      </button>
       <VideoList />
     </section>
   );
