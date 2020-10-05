966a5389bfedf4bd3256f853c2ca77d0d9d2aa88
oscar.castillo <oscar.castillo@wizeline.com>
2020-10-05 02:52:15 -0500

    Modified Nav to use Chakra UI components

--- src/components/Nav/Nav.component.jsx
+++ src/components/Nav/Nav.component.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { Box, Flex, Heading, IconButton, Button } from '@chakra-ui/core';
 
 import { useAuth } from '../../providers/Auth';
 import SearchVideo from '../SearchVideo/SearchVideo.component';
-
-import './Nav.styles.css';
 
 function Nav() {
   const history = useHistory();
@@ -16,39 +15,57 @@ function Nav() {
     history.push('/');
   }
 
+  function authenticate(event) {
+    event.preventDefault();
+    history.push('/login');
+  }
+
   return (
-    <div className="topnav">
-      <Link to="/">
-        home
-        <span role="img" aria-label="home">
-          🏠
-        </span>
-      </Link>
-      {authenticated ? (
-        <>
-          <Link to="/favorites">
-            favorites
-            <span role="img" aria-label="favorites">
-              ❤️
-            </span>
-          </Link>
-          <Link to="/" onClick={deAuthenticate}>
-            logout ←
-          </Link>
-        </>
-      ) : (
-        <>
-          <p>
-            favorites
-            <span role="img" aria-label="favorites">
-              ❤️
-            </span>
-          </p>
-          <Link to="/login">log in →</Link>
-        </>
-      )}
-      <SearchVideo />
-    </div>
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      color="white"
+    >
+      <Flex align="center" mr={5}>
+        <Link to="/">
+          <Heading as="h1" size="lg" letterSpacing="-.1rem">
+            WizeTube
+          </Heading>
+        </Link>
+      </Flex>
+      <Box
+        display={{ md: 'block' }}
+        width={{ md: 'auto' }}
+        alignItems="center"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+      >
+        <SearchVideo />
+      </Box>
+      <Box display={{ md: 'flex' }} mt={{ base: 4, md: 0 }}>
+        <Link to="/favorites">
+          <IconButton
+            aria-label="Favorites"
+            icon="star"
+            mt={{ base: 4, md: 0 }}
+            mr={6}
+            display={authenticated ? 'block' : 'none'}
+            bg="transparent"
+          />
+        </Link>
+        <Button
+          bg="transparent"
+          border="1px"
+          w="6rem"
+          onClick={authenticated ? deAuthenticate : authenticate}
+        >
+          {authenticated ? 'Log Out' : 'Log In'}
+        </Button>
+      </Box>
+    </Flex>
   );
 }
 
